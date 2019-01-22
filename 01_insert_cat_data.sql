cfcbc6a67d1505ad69330317efb753952fb7a6f9
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-22 22:17:24 +0000

    Automatically backed up by Learn

---
+++ 01_insert_cat_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+
+    INSERT INTO cats (name, age, breed, net_worth) VALUES ("Hana", 1, "Tabby", 21000);
+
+    INSERT INTO cats (name, age, breed, net_worth) VALUES ("Grumpy Cat", 4, "Persian", 181800);
+
+    INSERT INTO cats (name, age, breed, net_worth) VALUES ("Lil' Bub", 2, "Tortoiseshell", 3000000);
